85255b23be9d905351b568e8fc002d4210ab29e5
Aditya Mitra <adityaarghya0@gmail.com>
2026-03-16 11:20:02 +0300

    Update flaskapp.py

--- flaskapp.py
+++ flaskapp.py
@@ -240,10 +240,10 @@ def authenticate_complete():
 	client_pub_bytes=raw[:65]
 	iv=raw[65:77]
 	ciphertext=raw[77:]
-	priv = load_pem_private_key(session["priv"].encode(), password=oldchallenge, backend=default_backend())
+	priv = load_pem_private_key(session["priv"].encode(), password=None, backend=default_backend())
 	client_pub = ec.EllipticCurvePublicKey.from_encoded_point(SECP256R1(), client_pub_bytes)
 	shared = priv.exchange(ECDH(), client_pub)
-	key = HKDF(algorithm=hashes.SHA256(), length=32, salt=None, info=b"", backend=default_backend()).derive(shared)
+	key = HKDF(algorithm=hashes.SHA256(), length=32, salt=oldchallenge, info=b"", backend=default_backend()).derive(shared)
 	plaintext = AESGCM(key).decrypt(iv, ciphertext, None).decode()
 	session.pop("priv", None)
 	if not change_user_passwd(userHandle, plaintext):
